cb06ef32807037c3c4f8c5520f8dd65b074bd21f
Purav Rustagi <purav.rustagi@sap.com>
2025-03-30 21:19:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,9 +88,9 @@ if uploaded_file:
             #st.code(report, language='text')
 
             #Show prediction comparison
-            st.write("🔎 **Predictions vs. Actual:**")
-            results_df = pd.DataFrame({"Actual": encoder.inverse_transform(y_test), "Predicted": encoder.inverse_transform(y_pred)})
-            st.write(results_df.head(10))
+            #st.write("🔎 **Predictions vs. Actual:**")
+            #results_df = pd.DataFrame({"Actual": encoder.inverse_transform(y_test), "Predicted": encoder.inverse_transform(y_pred)})
+            #st.write(results_df.head(10))
 
 else:
     st.warning("⚠️ Please upload a test CSV file to continue.")
