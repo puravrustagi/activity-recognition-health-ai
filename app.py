30522517fd3ae1e8f5352c509e9b9ec95629ee25
Purav Rustagi <purav.rustagi@sap.com>
2025-03-30 22:12:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import joblib
 from sklearn.metrics import accuracy_score, classification_report
+from tensorflow.keras.optimizers import Adam
 
 import joblib
 from scikeras.wrappers import KerasClassifier
