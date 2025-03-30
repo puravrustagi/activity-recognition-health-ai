9a79f9374ed388682576ed5f41239d1c75689295
Purav Rustagi <purav.rustagi@sap.com>
2025-03-30 21:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if uploaded_file:
             #Show prediction comparison
             st.write("🔎 **Predictions vs. Actual:**")
             results_df = pd.DataFrame({"Actual": encoder.inverse_transform(y_test), "Predicted": encoder.inverse_transform(y_pred)})
-            st.write(results_df.head(5))
+            st.write(results_df.head(10))
 
 else:
     st.warning("⚠️ Please upload a test CSV file to continue.")
